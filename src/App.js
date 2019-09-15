7c7958f097df2f7203e804b5f488cd66b5ee1e07
Kyosuke Miyatake <34120910+znsz4c@users.noreply.github.com>
2019-09-16 01:15:38 +0900

    もっく
    ごめん

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import Console from './components/Console';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state={
+      value:0
+    }
   }
+
   render() {
     return (
-      <div><Console /></div>
+      <div>
+        <Console />
+      </div>
     );
   }
 }
--- src/components/Console.js
+++ src/components/Console.js
@@ -3,13 +3,18 @@ import React from 'react';
 export default class Console extends React.Component {
     constructor(props) {
         super(props);
-        
+        this.state={
+          value:0
+        }
     }
     
-
     render() {
         return (
-          <div>aaaaaa</div>
+          <div>
+            閾値
+            <input type="value" ref="inputText" defaultValue='0'></input>
+            <button>submit</button>
+          </div>
         );
       }
 }
